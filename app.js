881e5ae66157a54185cce2c0524e61cf429bb477
Ozysandias <96261311+Ozysandias@users.noreply.github.com>
2022-02-07 12:45:37 -0600

    fixed switch theme/ cookies

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ menu.addEventListener('click', function() {
     menuLinks.classList.toggle('active')
 });
 
+// CONTACT FORM 
+
 window.addEventListener("DOMContentLoaded", function () {
     // get the form elements defined in your form HTML above
   
@@ -33,22 +35,56 @@ window.addEventListener("DOMContentLoaded", function () {
       var data = new FormData(form);
       ajax(form.method, form.action, data, success, error);
     });
+});
+
+
+// helper function for sending an AJAX request  
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
+  
+// DARK/ LIGHT THEME TOGGLE 
+
+document.getElementById('checkbox').addEventListener('change', function(event){
+  (event.target.checked) ? document.body.setAttribute('data-theme', 'light') : document.body.removeAttribute('data-theme');
+});
+
+// REMEMBER THEME
+
+var themeSwitch = document.getElementById('checkbox');
+if(themeSwitch) {
+  initTheme(); // on page load, if user has already selected a specific theme -> apply it
+
+  themeSwitch.addEventListener('change', function(event){
+    resetTheme(); // update color theme
   });
-  
-  // helper function for sending an AJAX request
-  
-  function ajax(method, url, data, success, error) {
-    var xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState !== XMLHttpRequest.DONE) return;
-      if (xhr.status === 200) {
-        success(xhr.response, xhr.responseType);
-      } else {
-        error(xhr.status, xhr.response, xhr.responseType);
-      }
-    };
-    xhr.send(data);
-  }
-  
+
+  function initTheme() {
+    var lightThemeSelected = (localStorage.getItem('checkbox') !== null && localStorage.getItem('checkbox') === 'light');
+    // update checkbox
+    themeSwitch.checked = lightThemeSelected;
+    // update body data-theme attribute
+    lightThemeSelected ? document.body.setAttribute('data-theme', 'light') : document.body.removeAttribute('data-theme');
+  };
+
+  function resetTheme() {
+    if(themeSwitch.checked) { // dark theme has been selected
+      document.body.setAttribute('data-theme', 'light');
+      localStorage.setItem('checkbox', 'light'); // save theme selection 
+    } else {
+      document.body.removeAttribute('data-theme');
+      localStorage.removeItem('checkbox'); // reset theme selection 
+    } 
+  };
+}
